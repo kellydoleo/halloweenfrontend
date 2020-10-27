a02e03e1ae511eea9cc6293844b9f66d1a17441a
Christine Taylor <CTaylor100@gmail.com>
2020-10-27 18:54:48 -0400

    Fetch on App.js is connected to backend and console logs the DB

--- src/App.js
+++ src/App.js
@@ -4,92 +4,108 @@ import { Route, Link, Switch } from "react-router-dom";
 import Display from "./Display";
 import Form from "./Form";
 
+
+
+
 function App() {
-  //URL Variable
-  const url = "https://jszendogsbackend.herokuapp.com"
-  //state to hold API
-  const [dogs, setDogs] = React.useState([])
-  
-  const emptyDog = {
-    name: "",
-    age: 0,
-    img: ""
+//   //URL Variable - NEEDS TO BE UPDATED
+  const url = "http://localhost:3003"  //Update to a herokuapp.com
+  console.log('Current Base URL:', url);
+
+  fetch(url + '/costumes')
+  .then(data => {
+    return data.json()},
+    err => console.log(err))
+  .then(parsedData => console.log(parsedData),
+    err => console.log(err))
+
+  //state to hold API  
+  const emptyCostume = {
+    name: '',
+    price: '',
+    img: '',
+    type: []
   };
+
   //selected dog state to edit
-  const [selectedDog, setSelectedDog] = React.useState(emptyDog)
-  //Function to get dogs via API
-  const getDogs = () => {
-    fetch(url + "/dog")
+  const [costume, setCostume] = React.useState(emptyCostume)
+
+  // Function to get dogs via API
+  const getCostumes = () => {
+    fetch(url + "/costumes")
     .then(response => response.json())
     .then(data => {
       console.log(data)
-      setDogs(data)
+      setCostume(data)
     })
   }
 
+  
   //useEffect to do initial call
   React.useEffect(() => {
-    getDogs()
+    getCostumes()
   }, [])
 
-//handle to create dogs
-const handleCreate = (newDog) => {
-  fetch(url + "/dog", {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(newDog)
-  }).then(response => {
-    getDogs()
-  })
-}
-//handleUpdate function for Edit
-const handleUpdate = (dog) => {
-  fetch(url + "/dog/" + dog._id, {
-    method: "put",
-    headers: {
-      "Content-Type": "application/json"
-    }, body: JSON.stringify(dog)
-  }).then(response => getDogs())
-}
-//set state when you select dog to edit
-const selectDog = (dog) => {
-  setSelectedDog(dog);
-}
+// //handle to create dogs
+// const handleCreate = (newDog) => {
+//   fetch(url + "/dog", {
+//     method: "post",
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(newDog)
+//   }).then(response => {
+//     getDogs()
+//   })
+// }
+// //handleUpdate function for Edit
+// const handleUpdate = (dog) => {
+//   fetch(url + "/dog/" + dog._id, {
+//     method: "put",
+//     headers: {
+//       "Content-Type": "application/json"
+//     }, body: JSON.stringify(dog)
+//   }).then(response => getDogs())
+// }
+// //set state when you select dog to edit
+// const selectDog = (dog) => {
+//   setSelectedDog(dog);
+// }
 
-const deleteDog = (dog) => {
-  fetch(url + '/dog/' + dog._id, {
-    method: 'delete'
-  }).then(respnse => getDogs())
-}
+// const deleteDog = (dog) => {
+//   fetch(url + '/dog/' + dog._id, {
+//     method: 'delete'
+//   }).then(respnse => getDogs())
+// }
   return (
     <div className="App">
-      <h1>DOG LISTING SITE</h1>
-      <hr />
-      <Link to="/create">
-        <button>Add Dog</button>
-      </Link>
-      <main>
-        <Switch>
-          <Route exact path="/" render={(rp) => <Display selectDog={selectDog}{...rp} dogs = {dogs} deleteDog={deleteDog}/>} />
-          <Route
-            exact
-            path="/create"
-            render={(rp) => (
-              <Form {...rp} label="create" dog={emptyDog} handleSubmit={handleCreate} />
-            )}
-          />
-          <Route
-            exact
-            path="/edit"
-            render={(rp) => (
-              <Form {...rp} label="update" dog={selectedDog} handleSubmit={handleUpdate} />
-            )}
-          />
-        </Switch>
-      </main>
-    </div>
+      <h1>HALLOWEEN COSTUMES</h1>
+       <hr />
+
+
+       {/* <Link to="/create">
+//         <button>Add Dog</button>
+//       </Link>
+//       <main>
+//         <Switch>
+//           <Route exact path="/" render={(rp) => <Display selectDog={selectDog}{...rp} dogs = {dogs} deleteDog={deleteDog}/>} />
+//           <Route
+//             exact
+//             path="/create"
+//             render={(rp) => (
+//               <Form {...rp} label="create" dog={emptyDog} handleSubmit={handleCreate} />
+//             )}
+//           />
+//           <Route
+//             exact
+//             path="/edit"
+//             render={(rp) => (
+//               <Form {...rp} label="update" dog={selectedDog} handleSubmit={handleUpdate} />
+//             )}
+//           />
+//         </Switch> 
+//       </main> */}
+     </div>
   );
 }
 
