2be4f74bcd75a967669aac036dd221455424cdcd
jimmischeff <jimmischeff@gmail.com>
2020-10-28 16:39:49 -0400

    updated styling

--- src/Display.js
+++ src/Display.js
@@ -1,27 +1,29 @@
 import React from "react";
 import {Link} from 'react-router-dom'
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap'
 
 const Display = (props) => {
   const {costumes} = props
   
   const loaded = () => (
-    <div style={{textAlign: 'center'}}>
-      <Link to="/create">
-         <button>Add Costume</button>
-       </Link>
+    <div style={{textAlign: 'center', display: 'inline-flex'}}>
       {costumes.map((costume) => (
+        <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
         <article key={costume._id}>
-          <img src={costume.img}/>
-          <h1>{costume.name}</h1>
-          <h3>{costume.price}</h3>
-          <button onClick ={() => {
+          <CardTitle tag='h1'>{costume.name}</CardTitle>
+          <CardImg src={costume.img}/>
+          <CardBody>
+          <h3>${costume.price}</h3>
+          <Button onClick ={() => {
             props.selectCostume(costume)
             props.history.push('/edit')
-            }}>Edit</button>
-            <button onClick ={() => {
+            }}>Edit</Button>
+            <Button color='danger' onClick ={() => {
             props.deleteCostume(costume)
-            }}>Delete</button>
+            }}>Delete</Button>
+        </CardBody>
         </article>
+        </Card>
       ))}
     </div>
   )
